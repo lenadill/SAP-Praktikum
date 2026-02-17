856c305c197662b02ea7bcbe47540e3ef1783385
Flex6x <hanf.felix@outlook.de>
2026-02-17 11:36:43 +0100

    Eingabefeld eingerichtet

---
+++ Liste/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("openForm");
+const form = document.getElementById("formContainer");
+
+button.addEventListener("click", () => {
+  form.style.display = "block";
+});
